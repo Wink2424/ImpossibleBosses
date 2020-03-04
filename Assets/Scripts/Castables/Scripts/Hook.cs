110760c8190014e70f0e46f4639cd27d2b6800b7
Wyatt Olsen <wyattolsenmi@gmail.com>
2020-03-03 18:47:50 -0700

    Cleaned hook cs

--- Assets/Scripts/Castables/Scripts/Hook.cs
+++ Assets/Scripts/Castables/Scripts/Hook.cs
@@ -39,51 +39,59 @@ public class Hook : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (hookStart == new Vector3(0, 0, 0))
-        {
-            hookStart = new Vector3(hook.position.x, hook.position.y, hook.position.z);
-        }
+        //1: move hook forward 
+        //2: set hookStart/bouncePosition[0] to player Position as ... the return path.
+        //3: if hook range has been reached, move hook towards transform to last bouncePosition in list
+        //3a:   if last bouncePosition is 001f away, remove last bouncePosition in list.
+        //4: else hook range has not been reached, detect for collision
+        //4a: if hook collision occurs, add to bouncePosition[next]
+        //5: 
 
-        //Move hook forwards
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        //Rotate the hook
-
-        // <IMPLEMENT GRAPHIC ROTATION TO HOOK
-
-        // hook.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
-
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-        hookStart = new Vector3(GetPlayerPosition().transform.position.x, 
-            GetPlayerPosition().transform.position.y, GetPlayerPosition().transform.position.z);
-
+        int bounceCount = bouncePositions.Count - 1;
         if (bouncePositions.Count == 0)
         { //populate first array position
             bouncePositions.Add(new Vector3(0, 0, 0));
         }
+        hookStart = new Vector3(GetPlayerPosition().transform.position.x,
+            GetPlayerPosition().transform.position.y, GetPlayerPosition().transform.position.z);
+        bouncePositions[0] = hookStart; //assign index 0 to player position
+        //if (hookStart == new Vector3(0, 0, 0))
+        //{
+        //    hookStart = new Vector3(hook.position.x, hook.position.y, hook.position.z);
+        //}
+
+        //Move hook forwards
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Rotate the 
+        // <IMPLEMENT GRAPHIC ROTATION TO 
+        // hook.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
         
-        bouncePositions[0] = (new Vector3(playerObj.transform.position.x,
-         playerObj.transform.position.y,
-         playerObj.transform.position.z));
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .6f, collisionMask))
+            {
+                Debug.Log("Object collision");
+                Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+                float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, rot, 0);
 
-
-        if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .6f, collisionMask))
+            if (distanceTraveled < hookRange)
+            {
+                Vector3 temp = new Vector3(hook.position.x, hook.position.y, hook.position.z);
+                bouncePositions.Add(temp);
+            }
+            }
+        
+        else
         {
-            Debug.Log("Object collision");
-            Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
-            float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, rot, 0);
+            if(bounceCount != 0)
+            {
 
-
-            Vector3 temp = new Vector3(hook.position.x, hook.position.y, hook.position.z);
-          //  Debug.Log("Vector3 temp =  " + temp.ToString());
-            bouncePositions.Add(temp);
-           // Debug.Log(GetInstanceID().ToString());
-
-
+            }
         }
-        distanceTraveled = 1f;
 
+
+        
         for (int i = 1; i < bouncePositions.Count + 1; i++)
             {
             
@@ -93,34 +101,34 @@ public class Hook : MonoBehaviour {
                 
             }
             if (bouncePositions.Count > 1)
-                
                 distanceTraveled += Vector3.Distance
                     (new Vector3(hook.position.x, hook.position.y, hook.position.z)
                     , bouncePositions[i - 1]);
            
 
         }
-        //}
         
+        //}
 
-       // NEED TO REMOVE THE BOUNCE POINT IF WE HAVE REACHED A SPECIFIC RANGE USING VECTOR3.DISTANCE = .001F????? LINES 107 - 131
 
-        if (distanceTraveled > hookRange )
+        // NEED TO REMOVE THE BOUNCE POINT IF WE HAVE REACHED A SPECIFIC RANGE USING VECTOR3.DISTANCE = .001F????? LINES 107 - 131
+        /*
+        if (distanceTraveled >= hookRange)
         {
-            //  Debug.Log("Max Hook Distance Reached");
-            //  working
-            //  hook.position = Vector3.MoveTowards(hook.position, playerObj.transform.position, 5 * Time.deltaTime);
-            int bounceCount = bouncePositions.Count - 1;
-            if (bouncePositions.Count >= 1)
+            if (bouncePositions.Count == 0)
             {
                 Debug.Log(bounceCount);
+            }
+            else
+            {
+                Debug.Log(bounceCount + "else");
                 if (Vector3.Distance(bouncePositions[bounceCount], transform.position) > .01f)
                 {
                     bouncePositions.RemoveAt(bounceCount);
                 }
-                    transform.position = Vector3.MoveTowards(transform.position, bouncePositions[bounceCount], Time.deltaTime * speed);
+                transform.position = Vector3.MoveTowards(transform.position, bouncePositions[bounceCount], Time.deltaTime * speed);
             }
-
+            */
 
 
             //if (bouncePositions.Count > 1)
@@ -137,12 +145,7 @@ public class Hook : MonoBehaviour {
             //        }
             //    }
             //}
-
-        }
-        else
-        {
-            
-        }
+        
         
     }
 
@@ -157,3 +160,5 @@ public class Hook : MonoBehaviour {
 
 
 }
+
+
