39d536f1469798801d01684356297e0b9f5f5833
Tavail <Kadaj8899@gmail.com>
2020-03-03 21:49:09 -0700

    Added some hook logic for getting the hook to move and return to the player.

--- Assets/Scripts/Castables/Scripts/Hook.cs
+++ Assets/Scripts/Castables/Scripts/Hook.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class Hook : MonoBehaviour {
+public class Hook : MonoBehaviour
+{
 
-    
+
     public LayerMask collisionMask;
     public List<Vector3> bouncePositions = new List<Vector3>();
     public Transform hook;
@@ -12,7 +13,8 @@ public class Hook : MonoBehaviour {
     private float rotationSpeed = 800;
     private float hookRange = 15;
     private float distanceTraveled = 0;
-    Vector3 hookStart = new Vector3(0,0,0);
+    private bool traveling;
+    Vector3 hookStart = new Vector3(0, 0, 0);
     private GameObject playerObj = null;
 
 
@@ -22,134 +24,91 @@ public class Hook : MonoBehaviour {
         hookRange = hookrange;
     }
 
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
         //Starting position, to be updated to reference player transform
-        
+        if (bouncePositions.Count == 0)
+        {
+            bouncePositions.Add(new Vector3(0, 0, 0));
+        }
+
+
+        // set the initial state for the hook to traveling
+        traveling = true;
     }
-
-
 
 
     // Update is called once per frame
     void Update()
     {
-        //1: move hook forward 
-        //2: set hookStart/bouncePosition[0] to player Position as ... the return path.
-        //3: if hook range has been reached, move hook towards transform to last bouncePosition in list
-        //3a:   if last bouncePosition is 001f away, remove last bouncePosition in list.
-        //4: else hook range has not been reached, detect for collision
-        //4a: if hook collision occurs, add to bouncePosition[next]
-        //5: 
 
-        int bounceCount = bouncePositions.Count - 1;
-        if (bouncePositions.Count == 0)
-        { //populate first array position
-            bouncePositions.Add(new Vector3(0, 0, 0));
-        }
+        // Assign bouncePositions[0] to player's current position
         hookStart = new Vector3(GetPlayerPosition().transform.position.x,
             GetPlayerPosition().transform.position.y, GetPlayerPosition().transform.position.z);
-        bouncePositions[0] = hookStart; //assign index 0 to player position
-        //if (hookStart == new Vector3(0, 0, 0))
-        //{
-        //    hookStart = new Vector3(hook.position.x, hook.position.y, hook.position.z);
-        //}
+        bouncePositions[0] = hookStart;
 
-        //Move hook forwards
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        //Rotate the 
-        // <IMPLEMENT GRAPHIC ROTATION TO 
-        // hook.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
-        
+        // The hook is still moving from its initial throw
+        if (traveling)
+        {
+            //Move hook forwards
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+            // Check for bouncePositions
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .6f, collisionMask))
             {
-                Debug.Log("Object collision");
                 Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
                 float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, rot, 0);
 
-            if (distanceTraveled < hookRange)
-            {
-                Vector3 temp = new Vector3(hook.position.x, hook.position.y, hook.position.z);
-                bouncePositions.Add(temp);
+                if (distanceTraveled < hookRange)
+                {
+                    Vector3 temp = new Vector3(hook.position.x, hook.position.y, hook.position.z);
+                    bouncePositions.Add(temp);
+                }
             }
-            }
-        
+        }
         else
         {
-            if(bounceCount != 0)
+            // Grab last position in bouncePositions
+            var lastPosition = bouncePositions.LastOrDefault();
+
+            // Move this item towards the last bounce position
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(lastPosition.x, lastPosition.y, lastPosition.z), speed * Time.deltaTime);
+
+            // if the last position is where the hook is
+            if (transform.position == lastPosition)
             {
-
-            }
-        }
-
-
-        
-        for (int i = 1; i < bouncePositions.Count + 1; i++)
-            {
-            
-            if (bouncePositions.Count == 1)
-            {
-                distanceTraveled = Vector3.Distance(new Vector3(hook.position.x, hook.position.y, hook.position.z),hookStart);
-                
-            }
-            if (bouncePositions.Count > 1)
-                distanceTraveled += Vector3.Distance
-                    (new Vector3(hook.position.x, hook.position.y, hook.position.z)
-                    , bouncePositions[i - 1]);
-           
-
-        }
-        
-        //}
-
-
-        // NEED TO REMOVE THE BOUNCE POINT IF WE HAVE REACHED A SPECIFIC RANGE USING VECTOR3.DISTANCE = .001F????? LINES 107 - 131
-        /*
-        if (distanceTraveled >= hookRange)
-        {
-            if (bouncePositions.Count == 0)
-            {
-                Debug.Log(bounceCount);
-            }
-            else
-            {
-                Debug.Log(bounceCount + "else");
-                if (Vector3.Distance(bouncePositions[bounceCount], transform.position) > .01f)
+                // if the last position is the same as the only bounce position left
+                if (lastPosition == bouncePositions.FirstOrDefault())
                 {
-                    bouncePositions.RemoveAt(bounceCount);
+                    // Destroy the Hook Game Object
+                    Destroy(this.gameObject);
                 }
-                transform.position = Vector3.MoveTowards(transform.position, bouncePositions[bounceCount], Time.deltaTime * speed);
+
+                // Remove the last bounce position 
+                bouncePositions.RemoveAt(bouncePositions.Count - 1);
             }
-            */
+
+        }
 
 
-            //if (bouncePositions.Count > 1)
-            //{
-            //    //for (int i = bouncePositions.Count-1; i > 1;)
-            //    while(bouncePositions.Count >= 1)
-            //    {
-            //        int nextPosition = bouncePositions.Count - 1; //-1 to use as index
-            //        transform.position = Vector3.MoveTowards(transform.position, bouncePositions[nextPosition], Time.deltaTime * speed);
-            //        Debug.Log("Moving to " + bouncePositions[nextPosition] + " " + nextPosition);
-            //        if(.01f > Vector3.Distance(transform.position,bouncePositions[nextPosition]))
-            //        {
-            //            bouncePositions.RemoveAt(nextPosition);
-            //        }
-            //    }
-            //}
-        
-        
+        // Calculate the distance the hook is total from player (including each bounce)
+        distanceTraveled = 0;
+        bouncePositions.ForEach(x => distanceTraveled += Vector3.Distance(new Vector3(hook.position.x, hook.position.y, hook.position.z), x));
+
+        // if the distance traveled is more than the range of the player
+        if (distanceTraveled > hookRange)
+        {
+            // Start the returning process for the hook
+            traveling = false;
+        }
     }
 
-    
+
 
     private GameObject GetPlayerPosition()
     {
--- Assets/Scripts/Castables/Scripts/HookManager.cs
+++ Assets/Scripts/Castables/Scripts/HookManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HookManager : MonoBehaviour
 {
-    public Vector3 offset = new Vector3(0, .6f);
+    public Vector3 offset = new Vector3(0, .3f);
     public GameObject activeHook;
     public GameObject HookObject;
 
